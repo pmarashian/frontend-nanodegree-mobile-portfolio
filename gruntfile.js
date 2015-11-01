38bfe5200fa7e35a55667cc41a4811ae42074aad
pmarashian <phillip.marashian@gmail.com>
2015-11-01 10:55:48 -0800

    commit

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+// File modified from http://gruntjs.com/getting-started
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'views/**/*.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        cssmin: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'views/**/*.css',
+                dest: 'build/<%= pkg.name %>.min.css'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
