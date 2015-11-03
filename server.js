8ff5d0dce95a374d4f9e15b35c71efb4464ac30a
pmarashian <phillip.marashian@gmail.com>
2015-11-02 18:07:42 -0800

    Optimized resize pizza functions

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express               = require('express'),
 
 app.use( express.static( __dirname ) );
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
 
     var host = server.address().address;
     var port = server.address().port;
